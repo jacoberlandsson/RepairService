7d9f8f91d24b0097ad73df0efe178285ce5dc25e
Jacob Erlandsson <erlandssonjacob@hotmail.com>
2023-03-26 14:26:46 +0200

    created commentservice

--- RepairService/Program.cs
+++ RepairService/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RepairService.Services;
+
+var statusService = new StatusService();
+
+await statusService.StartAsync();
---
+++ RepairService/Services/StatusService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepairService.Contexts;
+using RepairService.Models.Entities;
+
+namespace RepairService.Services;
+
+internal class StatusService
+{
+    private readonly DataContext _context = new();
+
+    public async Task StartAsync()
+    {
+        if (!await _context.Statuses.AnyAsync())
+        {
+            var statuslist = new List<StatusEntity>()
+            {
+                new StatusEntity() { RepairStatus = "Ej påbörjad" },
+                new StatusEntity() { RepairStatus = "Pågående" },
+                new StatusEntity() { RepairStatus = "Avslutad "},
+            };
+
+            _context.AddRange(statuslist);
+            await _context.SaveChangesAsync();  
+        }
+    }
+
+    public async Task<IEnumerable<StatusEntity>> GetAllAsync()
+    {
+        return await _context.Statuses.ToListAsync();
+    }
+}
+
